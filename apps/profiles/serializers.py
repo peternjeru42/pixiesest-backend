66575340d73de842a71a7ee6f7fbb7ccf6844f7a
Peternjeru42 <petermushemi42@gmail.com>
2026-05-13 17:08:39 +0300

    Add email and profile photo URL fields to UserProfileSerializer

--- apps/profiles/serializers.py
+++ apps/profiles/serializers.py
@@ -4,6 +4,9 @@ from .models import UserProfile, UserProfileStats
 
 
 class UserProfileSerializer(serializers.ModelSerializer):
+    email = serializers.EmailField(source="user.email", read_only=True)
+    profile_photo_url = serializers.URLField(source="user.profile_photo_url", read_only=True)
+
     class Meta:
         model = UserProfile
         fields = "__all__"
